fc9ae90ccdee313881a0ca1defcfe98af0542d37
codingsake <thinthazin.chris@gmail.com>
2023-05-14 23:55:41 -0400

    Added NormatilizeURL func and test case

--- crawl.js
+++ crawl.js
@@ -1,3 +1,16 @@
-modeule.exports = {
+//this function return normalized URL
+function normalizeURL(url) {
+    const urlObj = new URL(url) //Parse URL string using the WHATWG API
+    let fullPath = `${urlObj.host}${urlObj.pathname}` //Store host and pathname
+    
+    //Check if the URL is valid 
+    if (fullPath.length > 0 && fullPath.slice(-1) === '/') {
+        fullPath = fullPath.slice(0, -1)
+    }
+    return fullPath
+
+}
+
+module.exports = {
     normalizeURL
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,8 +1,14 @@
-const {test, expect} = require('@jest/globals');
-
-const {normalizeURL} = require('./crawl.js');
+const { normalizeURL } = require('./crawl.js')
+const { test, expect } = require('@jest/globals')
 
 // Edge Cases
 //missing spelling
 // no input URL
 // more than one slash in path
+
+test('normalizeURL protocol', () => {
+    const input = 'https://blog.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'  
+    expect(actual).toEqual(expected)
+})
