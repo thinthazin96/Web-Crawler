28b09fbbb3d8646c702955f83371429dc8ae0bee
codingsake <thinthazin.chris@gmail.com>
2023-05-22 11:47:33 -0400

    Added crawlPage() and updated on main()

--- crawl.js
+++ crawl.js
@@ -35,18 +35,39 @@ function getURLsFromHTML(htmlBody, baseURL) {
     return urls
 }
 
-async function crawlPage(currentURL){
+async function crawlPage(baseURL, currentURL, pages){
 
+    // FOR EXTERNAL LINK
+    //baseURL is the domain of the website
+    const baseURLObj = new URL(baseURL)
+    //currentURL is the page we are currently crawling
+    const currentURLObj = new URL(currentURL)
+    //if domain is different from current, return pages we have visited
+    if (baseURLObj.hostname !== currentURLObj.hostname) {
+        return pages
+    }
+    
+    // FOR INTERNAL LINK
+    //normalize the current URL
+    const normalizedCurrentURL = normalizeURL(currentURL)
+    //If the pages object already has an entry for the normalized version of the current URL, just increment the count and return the current pages.
+    if (pages[normalizedCurrentURL] > 0){
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+
+    //set 1 for the first visit to new URL
+    pages[normalizedCurrentURL] = 1
     console.log(`actively crawling: ${currentURL}`)
+
 
     //Handle error gracefully when there is a bad link in the website
     try {
         const resp = await fetch(currentURL)
-
         //If the HTTP status code is an error level code, print an error and return
         if(resp.status > 399){
             console.log(`error in fetch with status code ${resp.status} on page: ${currentURL}`)
-            return 
+            return pages
         }
 
         //If the response content-type header isn't text/html print and error and return
@@ -54,16 +75,24 @@ async function crawlPage(currentURL){
         //Edge case: if the response content-type header has text/html and others content-type, we want return the body
         if(!contentType.includes("text/html")){
             console.log(`non html response, content type: ${contentType}, on page: ${currentURL}`)
-            return 
+            return pages
         }
 
         //resp.text() return as promise in text format
-        console.log(await resp.text()) 
-    } catch(err) {
+        const htmlBody = await resp.text()
+        const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+        //Recursively crawl each URL you found on the page and update the pages to keep an aggregate count
+        for (const nextURL of nextURLs) {
+            pages = await crawlPage(baseURL, nextURL, pages)
+        }
+
+        } catch(err) {
         console.log(`error in fetch: ${err.message}m on page: ${currentURL}`)
-    }
-    
+        }
+        //return the updated pages object
+        return pages
 }
+
 
 
 module.exports = {
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { crawlPage } = require('./crawl.js')
 
-function main(){
+async function main(){
 
     //If the number of CLI arguments is less than 1, print an error and exit.
     if (process.argv.length < 3){
@@ -13,10 +13,14 @@ function main(){
         process.exit(1)
       }
 
-      //If we have exactly one CLI argument, it's the "baseURL", so print some kind of message letting the user know the crawler is starting at that baseURL.
-      const baseURL = process.argv[2]
-    
-      console.log(`starting crawl of: ${baseURL}...`)
-      crawlPage(baseURL)
+    //If we have exactly one CLI argument, it's the "baseURL", so print some kind of message letting the user know the crawler is starting at that baseURL.
+    const baseURL = process.argv[2]
+  
+    console.log(`starting crawl of: ${baseURL}...`)
+    const pages = await crawlPage(baseURL, baseURL, {})
+
+    for(const page of Object.entries(pages)){
+      console.log(page)
+      }
 }
 main();
